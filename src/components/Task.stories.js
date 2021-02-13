bdf91e08029e614cfe4b60f2db996eb2bd2c2d22
ochuko <mancubsoft@gmail.com>
2021-02-13 21:16:13 +0100

    add redux

--- src/components/Task.stories.js
+++ src/components/Task.stories.js
@@ -9,7 +9,7 @@ export const Default = Template.bind({});
 Default.args = {
   task: {
     id: "1",
-    title: "test task",
+    title: "Test Task",
     state: "TASK_INBOX",
     updatedAt: new Date(2018, 0, 1, 9, 0),
   },
